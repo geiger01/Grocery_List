c9f044eaf13c3f5032c13aa8033d265ba3b3f7f0
geiger01 <tantangeiger@gmail.com>
2021-12-04 14:54:02 +0200

    simple server working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-
 const app = express();
 const PORT = 8000;
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
